8be52748c3353aae047a1e80209a2ed9ddf1a676
Robert Madigan <131868277+robmad93@users.noreply.github.com>
2024-11-23 21:41:23 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -52,8 +52,16 @@ function getRandomComputerResult() {
   
   };
   function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    resetGameBtn.style.display ="none";
+    optionsContainer.style.display ="block";
+    winnerMsgElement.innerText = "";
+    roundResultsMsg.innerText = "";
+};
   
-  };
   
   resetGameBtn.addEventListener("click", resetGame);
   
